ed0da26e72bede5e404ea28650213480d800cc66
r-o-b-o-t-o <ax.cocat@gmail.com>
2025-01-23 19:54:08 +0000

    chore: update eluna documentation

---
+++ pages/eluna/ElunaQuery/sidebar.js
@@ -0,0 +1,19 @@
+document.write(`
+  <a id="ElunaQuery:GetBool" class="fn" href="../ElunaQuery/GetBool.html">GetBool</a>
+  <a id="ElunaQuery:GetColumnCount" class="fn" href="../ElunaQuery/GetColumnCount.html">GetColumnCount</a>
+  <a id="ElunaQuery:GetDouble" class="fn" href="../ElunaQuery/GetDouble.html">GetDouble</a>
+  <a id="ElunaQuery:GetFloat" class="fn" href="../ElunaQuery/GetFloat.html">GetFloat</a>
+  <a id="ElunaQuery:GetInt16" class="fn" href="../ElunaQuery/GetInt16.html">GetInt16</a>
+  <a id="ElunaQuery:GetInt32" class="fn" href="../ElunaQuery/GetInt32.html">GetInt32</a>
+  <a id="ElunaQuery:GetInt64" class="fn" href="../ElunaQuery/GetInt64.html">GetInt64</a>
+  <a id="ElunaQuery:GetInt8" class="fn" href="../ElunaQuery/GetInt8.html">GetInt8</a>
+  <a id="ElunaQuery:GetRow" class="fn" href="../ElunaQuery/GetRow.html">GetRow</a>
+  <a id="ElunaQuery:GetRowCount" class="fn" href="../ElunaQuery/GetRowCount.html">GetRowCount</a>
+  <a id="ElunaQuery:GetString" class="fn" href="../ElunaQuery/GetString.html">GetString</a>
+  <a id="ElunaQuery:GetUInt16" class="fn" href="../ElunaQuery/GetUInt16.html">GetUInt16</a>
+  <a id="ElunaQuery:GetUInt32" class="fn" href="../ElunaQuery/GetUInt32.html">GetUInt32</a>
+  <a id="ElunaQuery:GetUInt64" class="fn" href="../ElunaQuery/GetUInt64.html">GetUInt64</a>
+  <a id="ElunaQuery:GetUInt8" class="fn" href="../ElunaQuery/GetUInt8.html">GetUInt8</a>
+  <a id="ElunaQuery:IsNull" class="fn" href="../ElunaQuery/IsNull.html">IsNull</a>
+  <a id="ElunaQuery:NextRow" class="fn" href="../ElunaQuery/NextRow.html">NextRow</a>
+`);
