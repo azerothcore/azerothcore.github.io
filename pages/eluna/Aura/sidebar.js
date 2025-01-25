8c7c85820fa3ff7772374882e9a5dc39658a50a1
r-o-b-o-t-o <ax.cocat@gmail.com>
2025-01-25 14:49:20 +0000

    chore: update eluna documentation

---
+++ pages/eluna/Aura/sidebar.js
@@ -0,0 +1,14 @@
+document.write(`
+  <a id="Aura:GetAuraId" class="fn" href="../Aura/GetAuraId.html">GetAuraId</a>
+  <a id="Aura:GetCaster" class="fn" href="../Aura/GetCaster.html">GetCaster</a>
+  <a id="Aura:GetCasterGUID" class="fn" href="../Aura/GetCasterGUID.html">GetCasterGUID</a>
+  <a id="Aura:GetCasterLevel" class="fn" href="../Aura/GetCasterLevel.html">GetCasterLevel</a>
+  <a id="Aura:GetDuration" class="fn" href="../Aura/GetDuration.html">GetDuration</a>
+  <a id="Aura:GetMaxDuration" class="fn" href="../Aura/GetMaxDuration.html">GetMaxDuration</a>
+  <a id="Aura:GetOwner" class="fn" href="../Aura/GetOwner.html">GetOwner</a>
+  <a id="Aura:GetStackAmount" class="fn" href="../Aura/GetStackAmount.html">GetStackAmount</a>
+  <a id="Aura:Remove" class="fn" href="../Aura/Remove.html">Remove</a>
+  <a id="Aura:SetDuration" class="fn" href="../Aura/SetDuration.html">SetDuration</a>
+  <a id="Aura:SetMaxDuration" class="fn" href="../Aura/SetMaxDuration.html">SetMaxDuration</a>
+  <a id="Aura:SetStackAmount" class="fn" href="../Aura/SetStackAmount.html">SetStackAmount</a>
+`);
