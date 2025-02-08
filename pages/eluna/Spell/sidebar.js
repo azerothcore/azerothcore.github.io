1d3d8f31c49e3fa8d4c4ef3e51d77bde70f1960a
r-o-b-o-t-o <ax.cocat@gmail.com>
2025-02-08 06:36:53 +0000

    chore: update eluna documentation

---
+++ pages/eluna/Spell/sidebar.js
@@ -0,0 +1,15 @@
+document.write(`
+  <a id="Spell:Cancel" class="fn" href="../Spell/Cancel.html">Cancel</a>
+  <a id="Spell:Cast" class="fn" href="../Spell/Cast.html">Cast</a>
+  <a id="Spell:Finish" class="fn" href="../Spell/Finish.html">Finish</a>
+  <a id="Spell:GetCastTime" class="fn" href="../Spell/GetCastTime.html">GetCastTime</a>
+  <a id="Spell:GetCaster" class="fn" href="../Spell/GetCaster.html">GetCaster</a>
+  <a id="Spell:GetDuration" class="fn" href="../Spell/GetDuration.html">GetDuration</a>
+  <a id="Spell:GetEntry" class="fn" href="../Spell/GetEntry.html">GetEntry</a>
+  <a id="Spell:GetPowerCost" class="fn" href="../Spell/GetPowerCost.html">GetPowerCost</a>
+  <a id="Spell:GetReagentCost" class="fn" href="../Spell/GetReagentCost.html">GetReagentCost</a>
+  <a id="Spell:GetTarget" class="fn" href="../Spell/GetTarget.html">GetTarget</a>
+  <a id="Spell:GetTargetDest" class="fn" href="../Spell/GetTargetDest.html">GetTargetDest</a>
+  <a id="Spell:IsAutoRepeat" class="fn" href="../Spell/IsAutoRepeat.html">IsAutoRepeat</a>
+  <a id="Spell:SetAutoRepeat" class="fn" href="../Spell/SetAutoRepeat.html">SetAutoRepeat</a>
+`);
